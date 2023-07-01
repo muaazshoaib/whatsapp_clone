053e86654557e0b9582a58e0b8438f3d7ada631c
muaazshoaib <muaazshoaib1670@gmail.com>
2023-07-01 17:17:44 +0500

    Flutter Firebase App Connection

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'colors.dart';
+import 'firebase_options.dart';
 import 'screens/mobile_layout_screen.dart';
 import 'screens/web_layout_screen.dart';
 import 'utils/responsive_layout.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -15,7 +21,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Whatsapp Clone',
+      title: 'WhatsApp Clone',
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: backgroundColor,
       ),
