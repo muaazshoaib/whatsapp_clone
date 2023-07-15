899d32aefad79caaeedfa4977b2007b83d482ba3
muaazshoaib <muaazshoaib1670@gmail.com>
2023-07-15 19:20:48 +0500

    User Information Screen UI

--- lib/common/utils/utils.dart
+++ lib/common/utils/utils.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'dart:io';
+
+import 'package:image_picker/image_picker.dart';
 
 void showSnackBar({
   required BuildContext context,
@@ -9,4 +12,20 @@ void showSnackBar({
       content: Text(content),
     ),
   );
+}
+
+Future<File?> pickImageFromGallery(BuildContext context) async {
+  File? image;
+  try {
+    final pickImage = await ImagePicker().pickImage(
+      source: ImageSource.gallery,
+    );
+    if (pickImage != null) {
+      image = File(pickImage.path);
+    }
+  } catch (e) {
+    showSnackBar(context: context, content: e.toString());
+  }
+
+  return image;
 }
--- lib/features/auth/screens/user_information_screen.dart
+++ lib/features/auth/screens/user_information_screen.dart
@@ -1,12 +1,90 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-class UserInformationScreen extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:whatsapp_clone/common/utils/utils.dart';
+
+class UserInformationScreen extends StatefulWidget {
   static const String routeName = '/user-information';
 
   const UserInformationScreen({super.key});
 
   @override
+  State<UserInformationScreen> createState() => _UserInformationScreenState();
+}
+
+class _UserInformationScreenState extends State<UserInformationScreen> {
+  final TextEditingController nameController = TextEditingController();
+  File? image;
+
+  @override
+  void dispose() {
+    super.dispose();
+    nameController.dispose();
+  }
+
+  void selectImage() async {
+    image = await pickImageFromGallery(context);
+    setState(() {});
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    final size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              Stack(
+                children: [
+                  image == null
+                      ? const CircleAvatar(
+                          radius: 64,
+                          backgroundImage: NetworkImage(
+                            'https://med.gov.bz/wp-content/uploads/2020/08/dummy-profile-pic.jpg',
+                          ),
+                        )
+                      : CircleAvatar(
+                          radius: 64,
+                          backgroundImage: FileImage(
+                            image!,
+                          ),
+                        ),
+                  Positioned(
+                    bottom: -10,
+                    left: 80,
+                    child: IconButton(
+                      onPressed: selectImage,
+                      icon: const Icon(Icons.add_a_photo),
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  Container(
+                    width: size.width * 0.85,
+                    padding: const EdgeInsets.all(20),
+                    child: TextField(
+                      controller: nameController,
+                      decoration: const InputDecoration(
+                        hintText: 'Enter your name',
+                      ),
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.done,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
