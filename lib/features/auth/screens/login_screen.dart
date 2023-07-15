cc69bdbc5cdfb617bae445bf5e625dc3dd31ba60
muaazshoaib <muaazshoaib1670@gmail.com>
2023-07-15 17:22:40 +0500

    OTP Screen UI

--- lib/features/auth/screens/login_screen.dart
+++ lib/features/auth/screens/login_screen.dart
@@ -1,6 +1,8 @@
 import 'package:country_picker/country_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:whatsapp_clone/colors.dart';
+import 'package:whatsapp_clone/common/utils/utils.dart';
 import 'package:whatsapp_clone/common/widgets/custom_button.dart';
 import 'package:whatsapp_clone/features/auth/controller/auth_controller.dart';
 
@@ -40,6 +42,8 @@ class _LoginScreenState extends ConsumerState<LoginScreen> {
             context,
             '+${_country!.phoneCode}$phoneNumber',
           );
+    } else {
+      showSnackBar(context: context, content: 'Fill out all the fields.');
     }
   }
 
@@ -51,43 +55,48 @@ class _LoginScreenState extends ConsumerState<LoginScreen> {
       appBar: AppBar(
         title: const Text('Enter your phone number'),
         elevation: 0,
+        centerTitle: true,
+        backgroundColor: backgroundColor,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(18.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const Text('WhatsApp will need to verify your phone number.'),
-            const SizedBox(height: 10),
-            TextButton(
-              onPressed: pickCountry,
-              child: const Text('Pick Country'),
-            ),
-            const SizedBox(height: 5),
-            Row(
-              children: [
-                if (_country != null) Text('+${_country!.phoneCode}'),
-                const SizedBox(width: 10),
-                SizedBox(
-                  width: size.width * 0.7,
-                  child: TextField(
-                    controller: phoneController,
-                    decoration: const InputDecoration(
-                      hintText: 'phone number',
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(18.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Text('WhatsApp will need to verify your phone number.'),
+              const SizedBox(height: 10),
+              TextButton(
+                onPressed: pickCountry,
+                child: const Text('Pick Country'),
+              ),
+              const SizedBox(height: 5),
+              Row(
+                children: [
+                  if (_country != null) Text('+${_country!.phoneCode}'),
+                  const SizedBox(width: 10),
+                  SizedBox(
+                    width: size.width * 0.7,
+                    child: TextField(
+                      controller: phoneController,
+                      decoration: const InputDecoration(
+                        hintText: 'phone number',
+                      ),
+                      keyboardType: TextInputType.number,
                     ),
                   ),
-                ),
-              ],
-            ),
-            SizedBox(height: size.height * 0.6),
-            SizedBox(
-              width: 90,
-              child: CustomButton(
-                onPressed: () {},
-                text: 'NEXT',
+                ],
               ),
-            )
-          ],
+              SizedBox(height: size.height * 0.6),
+              SizedBox(
+                width: 90,
+                child: CustomButton(
+                  onPressed: sendPhoneNumber,
+                  text: 'NEXT',
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
--- lib/features/auth/screens/otp_screen.dart
+++ lib/features/auth/screens/otp_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:whatsapp_clone/colors.dart';
 
 class OTPScreen extends StatefulWidget {
   static const String routeName = '/otp-screen';
@@ -15,6 +16,37 @@ class OTPScreen extends StatefulWidget {
 class _OTPScreenState extends State<OTPScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    final size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Verifying your number'),
+        elevation: 0,
+        centerTitle: true,
+        backgroundColor: backgroundColor,
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            const SizedBox(height: 20),
+            const Text('We have sent an SMS with a code.'),
+            SizedBox(
+              width: size.width * 0.5,
+              child: TextField(
+                textAlign: TextAlign.center,
+                decoration: const InputDecoration(
+                  hintText: '- - - - - -',
+                  hintStyle: TextStyle(
+                    fontSize: 30,
+                  ),
+                ),
+                keyboardType: TextInputType.number,
+                onChanged: (value) {},
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
