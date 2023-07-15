0acbcfa8673a93443f09bf819b20e9fe3459c430
muaazshoaib <muaazshoaib1670@gmail.com>
2023-07-15 12:23:34 +0500

    Adding Riverpod

--- lib/features/auth/repository/auth_repository.dart
+++ lib/features/auth/repository/auth_repository.dart
@@ -1,24 +1,32 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:whatsapp_clone/common/utils/utils.dart';
 import 'package:whatsapp_clone/features/auth/screens/otp_screen.dart';
 
+final authRepositoryProvider = Provider(
+  (ref) => AuthRepository(
+    firebaseAuth: FirebaseAuth.instance,
+    firebaseFirestore: FirebaseFirestore.instance,
+  ),
+);
+
 class AuthRepository {
-  final FirebaseAuth auth;
-  final FirebaseFirestore firestore;
+  final FirebaseAuth firebaseAuth;
+  final FirebaseFirestore firebaseFirestore;
 
   AuthRepository({
-    required this.auth,
-    required this.firestore,
+    required this.firebaseAuth,
+    required this.firebaseFirestore,
   });
 
   void signInWithPhone(BuildContext context, String phoneNumber) async {
     try {
-      await auth.verifyPhoneNumber(
+      await firebaseAuth.verifyPhoneNumber(
         phoneNumber: phoneNumber,
         verificationCompleted: (PhoneAuthCredential phoneAuthCredential) async {
-          await auth.signInWithCredential(phoneAuthCredential);
+          await firebaseAuth.signInWithCredential(phoneAuthCredential);
         },
         codeSent: (String verificationId, int? forceResendingToken) async {
           Navigator.pushNamed(
