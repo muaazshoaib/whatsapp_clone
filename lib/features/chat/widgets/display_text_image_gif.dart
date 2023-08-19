8c4c00505b1e69b79626329d7bfb4485a2fdc774
muaazshoaib <muaazshoaib1670@gmail.com>
2023-08-19 12:42:39 +0500

    Playing Audio

--- lib/features/chat/widgets/display_text_image_gif.dart
+++ lib/features/chat/widgets/display_text_image_gif.dart
@@ -1,3 +1,4 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:whatsapp_clone/common/enums/message_enum.dart';
@@ -15,6 +16,9 @@ class DisplayTextImageGIF extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    bool isPlaying = false;
+    final AudioPlayer audioPlayer = AudioPlayer();
+
     return type == MessageEnum.text
         ? Text(
             message,
@@ -22,16 +26,38 @@ class DisplayTextImageGIF extends StatelessWidget {
               fontSize: 16,
             ),
           )
-        : type == MessageEnum.video
-            ? VideoPlayerItem(
-                videoUrl: message,
-              )
-            : type == MessageEnum.gif
-                ? CachedNetworkImage(
-                    imageUrl: message,
+        : type == MessageEnum.audio
+            ? StatefulBuilder(builder: (context, setState) {
+                return IconButton(
+                  constraints: const BoxConstraints(minWidth: 100),
+                  onPressed: () async {
+                    if (isPlaying) {
+                      await audioPlayer.pause();
+
+                      setState(() {
+                        isPlaying = false;
+                      });
+                    } else {
+                      await audioPlayer.play(UrlSource(message));
+                      setState(() {
+                        isPlaying = true;
+                      });
+                    }
+                  },
+                  icon:
+                      Icon(isPlaying ? Icons.pause_circle : Icons.play_circle),
+                );
+              })
+            : type == MessageEnum.video
+                ? VideoPlayerItem(
+                    videoUrl: message,
                   )
-                : CachedNetworkImage(
-                    imageUrl: message,
-                  );
+                : type == MessageEnum.gif
+                    ? CachedNetworkImage(
+                        imageUrl: message,
+                      )
+                    : CachedNetworkImage(
+                        imageUrl: message,
+                      );
   }
 }
